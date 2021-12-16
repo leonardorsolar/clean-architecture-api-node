993008371c8d882d3077ace3403c8f947da517cc
adm@1 <adm@cptsoftware.com.br>
2021-12-16 11:39:08 -0300

    fix: error server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,6 @@
 import 'module-alias/register'
-import app from "./config/app";
+import app from './config/app'
 
 app.listen(5000, () => {
-    console.log('Server runnin at http://localhost/5000')
+  console.log('Server runnin at http://localhost/5000')
 })
